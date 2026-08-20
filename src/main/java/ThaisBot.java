51ab669eedb3c8b076e6058a9262f7a7cd7e0edf
ThaiNguyen0806 <nguyenhoangthai080607@gmail.com>
2026-08-21 01:32:22 +0800

    Add echo behavior for Level-1

--- src/main/java/ThaisBot.java
+++ src/main/java/ThaisBot.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class ThaisBot {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Thai's Bot.");
         System.out.println("What can I do for you today?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String userInput = scanner.nextLine();
+            System.out.println(userInput);
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
     }
 }
