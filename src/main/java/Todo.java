9581c731c90bf9b98ac4989f3a4622100a480048
ThaiNguyen0806 <nguyenhoangthai080607@gmail.com>
2026-08-28 00:16:21 +0800

    Add task persistence to disk for Level-7

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -7,4 +7,10 @@ public class Todo extends Task {
     public String toString() {
         return "[T]" + super.toString();
     }
+
+    @Override
+    public String toFileString() {
+        String doneFlag = status == TaskStatus.DONE ? "1" : "0";
+        return "T | " + doneFlag + " | " + description;
+    }
 }
